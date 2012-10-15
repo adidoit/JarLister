6bc1172c05a8b01ec2cd9cb9aed50f84bdbff3bf
aditya pradhan <pradhan.aditya@gmail.com>
2012-10-15 10:34:28 -0700

    formatted html output to be indented

--- jarlister.rb
+++ jarlister.rb
@@ -9,18 +9,18 @@ jar_dir = gets.chomp
 Dir.chdir(jar_dir)
 jar_list = Dir.glob("*.jar")
 f_out = File.open('jar_list.html','w')
+f_out.write("<!DOCTYPE html>\n<html>\n")
 jar_list.each do |jar_file|
 	puts "\nJAR: " + jar_file + "\n"
 	cmd = "jar -tf " + jar_file
-	cmd_results =  %x[#{cmd}].inspect.to_s
-	cmd_results = cmd_results.gsub("\"","")
-	cmd_results = cmd_results.gsub("\\n","\n")
+	cmd_results =  %x[#{cmd}].inspect.to_s.gsub("\"","").gsub("\\n","\n").gsub("/",".")
 	array_of_results = cmd_results.split("\n")
-	f_out.write("<h1>" + jar_file + "</h1>")
-	f_out.write("<ul>")
+	f_out.write("<h1>" + jar_file + "</h1>\n")
+	f_out.write("<ul>\n")
 	array_of_results.each do |result|
-		f_out.write("<li>" + result + "</li>")
+		f_out.write("\t<li>" + result + "</li>\n")
 	end
-	f_out.write("</ul>")
+	f_out.write("</ul>\n")
 end
+f_out.write("</html>")
 f_out.close
